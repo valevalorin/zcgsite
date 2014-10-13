518ad09c969350f7fb94f8b00190d4c8a1b6be8f
Duncan Kelly <leucochroic@gmail.com>
2014-10-13 09:12:32 -0500

    Calendar fulling functional, began work on articles index and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,11 +13,34 @@ class ArticlesController < ApplicationController
 	end
 
 	def index
-	  @articles = Article.all
+	  #Process offset, determine range of articles to pull
+	  @offset = nil
+	  @prev = nil
+	  @next = 1
+	  stop = Article.last.id
+	  if params[:offset]
+	  	#Nav
+	  	@offset = Integer(params[:offset])
+	  	@offset = 0 if @offset < 0
+	  	@prev = @offset - 1 if @offset > 0
+	  	@next = @offset + 1
+
+	  	#Pull range
+	  	stop = stop - (@offset * 15)
+	  end	  
+	  start = stop - 15
+
+	  #Pull articles and associate with authors
+	  @articles = Article.where(id: start..stop)
+	  @articles.each do |article|
+	  	author = Author.find_by(id: article.author_id)
+	  	article.author = author
+	  end
 	end
 
 	def show
   		@article = Article.find(params[:id])
+  		@author = Article.find_by(id: @article.author_id)
 	end
 
 	def edit
--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,12 +3,26 @@ class CalendarController < ApplicationController
 	COMMON_YEAR_DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 	def index
+		#Get month offset and parse it if it exists. If less than zero than set to 0
+		@offset = nil
+		@prevMonth = nil
+		@nextMonth = 1
+		if params[:offset]
+			@offset = Integer(params[:offset])
+			@offset = 0 if @offset < 0
+			@prevMonth = @offset - 1 if @offset > 0
+			@nextMonth = @offset + 1
+		end
+
+		#Calculate current month and start date based on offset
 		curr_time = Time.now
-		@monthString = curr_time.strftime("%B")
 		date = Date.new(curr_time.year, curr_time.month, 1)
+		date = date + @offset.months if @offset
+		@monthString = date.strftime("%B")
 		starting_day = date.wday
 		days = days_in_month(date.month)
 
+		#Initialize structures to populate view
 		@numerals = []
 
 		@event_days = [] 
